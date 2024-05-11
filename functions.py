246fe35f4f2b1bd673a371bfea043766be7af4e5
Hazel Viswanath <viswanathsam@gmail.com>
2024-05-11 17:56:56 -0600

    Comments, a bit of refactor, and several new triangles.

--- functions.py
+++ functions.py
@@ -2,34 +2,35 @@ import numpy
 import math
 
 
-def getNewLine(oldLine):
+def getNewLine(oldLine: object) -> object:
+    # Get coordinates of start and end of old line.
     coords = oldLine[0].get_data()
     print(coords)
-
+    # Store Coordinates in variables.
     x1 = coords[0][0]
     x2 = coords[0][1]
     y1 = coords[1][0]
     y2 = coords[1][1]
-
+    # Calculate slope of the old line.
     m = (y2 - y1) / (x2 - x1)
-
+    # Get perpendicular slope.
     m2 = numpy.reciprocal(m)
     m3 = m2 * -1
     print(m)
     print(m2)
     print(m3)
-    slopeAngle = math.atan(m3)
-    print(slopeAngle)
-    angle = slopeAngle
+    # Change Slope into Degrees from x-axis.
+    angle = math.atan(m3)
     print(angle)
-
+    # Take the imaginary triangle's side lengths from angle and length of hypotenuse: 3.
     offset_x = math.cos(angle) * 3
     print(offset_x)
     offset_y = math.sin(angle) * 3
     print(offset_y)
-
+    # Use distance from endpoint of last line to calculate final x/y for new line.
     x2 = x1 - offset_x
     x = [x2, x1]
     y2 = y1 - offset_y
     y = [y2, y1]
+    # Send back the x and y to main drawing file.
     return [x, y]
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 import matplotlib.pyplot as plt
 import functions
+
 origin = [12, 10]
-##A
+# Draw
 a1 = plt.plot([15, 12], [10, 10])
 
-##B
 b1 = plt.plot([15, 15], [10, 13])
 
-##C
 c1 = plt.plot([15, 12], [13, 10])
 
+# Draw Lines
 newCoords = functions.getNewLine(c1)
-
 b2 = plt.plot(newCoords[0], newCoords[1])
 nCoords = b2[0].get_data()
-c2 = plt.plot([nCoords[0][0],origin[0]], [nCoords[1][0],origin[1]])
+c2 = plt.plot([nCoords[0][0], origin[0]], [nCoords[1][0], origin[1]])
 
 newCoords = functions.getNewLine(c2)
 b3 = plt.plot(newCoords[0], newCoords[1])
 nCoords = b3[0].get_data()
-c3 = plt.plot([nCoords[0][0],origin[0]], [nCoords[1][0],origin[1]])
+c3 = plt.plot([nCoords[0][0], origin[0]], [nCoords[1][0], origin[1]])
 
-# square plot
+newCoords = functions.getNewLine(c3)
+b4 = plt.plot(newCoords[0], newCoords[1])
+nCoords = b4[0].get_data()
+c4 = plt.plot([nCoords[0][0], origin[0]], [nCoords[1][0], origin[1]])
+
+newCoords = functions.getNewLine(c4)
+b5 = plt.plot(newCoords[0], newCoords[1])
+nCoords = b5[0].get_data()
+c5 = plt.plot([nCoords[0][0], origin[0]], [nCoords[1][0], origin[1]])
+
+newCoords = functions.getNewLine(c5)
+b6 = plt.plot(newCoords[0], newCoords[1])
+nCoords = b6[0].get_data()
+c6 = plt.plot([nCoords[0][0], origin[0]], [nCoords[1][0], origin[1]])
+
+newCoords = functions.getNewLine(c6)
+b7 = plt.plot(newCoords[0], newCoords[1])
+nCoords = b7[0].get_data()
+c7 = plt.plot([nCoords[0][0], origin[0]], [nCoords[1][0], origin[1]])
+
+# Make square plot
 plt.axis('square')
 
-##Draw to Screen
+# Draw to Screen
 plt.show()
